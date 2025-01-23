192ed78d5d28748587cb18c5c7f50074debdaa0c
LittleMoki <steamlittle659@gmail.com>
2025-01-24 01:34:06 +0500

    done

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
 	return (
 		<header className='container overflow-hidden h-[160px] relative mx-auto max-w-[600px]'>
-			<div className='absolute left-0 top-0 w-full h-full bg-cover bg-[url("./default-bg.svg")]'></div>
+			<div className='absolute left-0 top-0 w-full h-full bg-cover bg-[url("https://sentai.uz/default-bg.svg")]'></div>
 		</header>
 	)
 }
